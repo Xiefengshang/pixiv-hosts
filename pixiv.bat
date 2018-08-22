596729a24e8fa181338e19c7e5c1633b7b90f28e
SNSLogty <1092352939@qq.com>
2018-08-22 22:05:27 +0800

    Update pixiv.bat

--- pixiv.bat
+++ pixiv.bat
@@ -6,9 +6,9 @@ cls
 if exist %systemdrive%\Windows\System32\drivers\etc\hosts (echo hosts check successful) else (goto err)
 if exist aria2c.exe (goto main) else (goto noaria)
 :main
-echo 注意:用管理员权限打开
-echo 1.加载p站hosts并备份原来的hosts
-echo 2.恢复原来的hosts(先有备份)
+echo 娉ㄦ剰:鐢ㄧ鐞嗗憳鏉冮檺鎵撳紑
+echo 1.鍔犺浇p绔檋osts骞跺浠藉師鏉ョ殑hosts
+echo 2.鎭㈠鍘熸潵鐨刪osts(鍏堟湁澶囦唤)
 choice /c 12 /n
 if %errorlevel%==1 (goto cho1)
 if %errorlevel%==2 (goto cho2) 
@@ -32,6 +32,54 @@ if exist backup\bchosts (goto recover1) else (echo nobackup)
 goto main
 :recover1
 move %~dp0backup\bchosts C:\Windows\System32\drivers\etc\hosts
+echo done2
+pause
+exit
+:noaria
+echo no aria2c.exe
+pause
+:err
+echo error
+pause
+@echo off 
+md backup
+md download
+title pixiv-hosts verion0.1 by snslogty
+mode con cols=50 lines=26
+cls
+if exist %systemdrive%\Windows\System32\drivers\etc\hosts (echo hosts check successful) else (goto err)
+if exist aria2c.exe (goto main) else (goto noaria)
+:main
+del download\*.md
+pause
+aria2c.exe -d download https://raw.githubusercontent.com/SNSLogty/pixiv-hosts/master/hosts.md
+aria2c.exe -d download https://raw.githubusercontent.com/SNSLogty/pixiv-hosts/master/host2.md
+cls
+echo 娉ㄦ剰:鐢ㄧ鐞嗗憳鏉冮檺鎵撳紑
+echo 1.鍔犺浇p绔檋osts骞跺浠藉師鏉ョ殑hosts
+echo 2.鎭㈠鍘熸潵鐨刪osts(鍏堟湁澶囦唤)
+choice /c 12 /n
+if %errorlevel%==1 (goto cho1)
+if %errorlevel%==2 (goto cho2) 
+:cho1
+set webhost =https://raw.githubusercontent.com/SNSLogty/pixiv-hosts/master/hosts
+goto create
+:create
+aria2c.exe -d download https://raw.githubusercontent.com/SNSLogty/pixiv-hosts/master/hosts
+if exist hosts (echo download complete) else (goto err)
+goto create1
+:create1
+copy %systemdrive%\Windows\System32\drivers\etc\hosts backup\bchosts
+move /y %~dp0hosts C:\Windows\System32\drivers\etc\
+echo done 
+pause
+exit
+:cho2
+if exist backup\bchosts (goto recover1) else (echo nobackup)
+goto main
+:recover1
+cd 
+move /y %~dp0backup\bchosts C:\Windows\System32\drivers\etc\hosts
 echo done2
 pause
 exit
